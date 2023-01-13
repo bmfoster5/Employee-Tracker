c96c1985e5463be4b720d4e460d3b5882a0ea5f5
bmfoster5 <foster.brandon96@gmail.com>
2023-01-12 22:05:30 -0500

    added promts

--- Main/index.js
+++ Main/index.js
@@ -3,10 +3,79 @@ const inquirer = require('inquirer');
 function employeeManager() {
     console.log('WELCOME TO EMPLOYEE TRACKER');
     inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'actions',
-          choices: ["View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role"],
-        },
-      ])};
+        .prompt([
+            {
+                type: 'list',
+                name: 'actions',
+                message: "What would you like to do?",
+                choices: ["View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role"]
+
+            },
+            {
+                type: 'input',
+                name: 'addDepartment',
+                message: "What is the name of the department you would like to add?",
+                when: (answers) => answers['actions'] === 'Add Department'
+
+            },
+            {
+                type: 'input',
+                name: 'addRoleName',
+                message: "What is the name of the role you would like to add?",
+                when: (answers) => answers['actions'] === 'Add Role'
+
+            },
+            {
+                type: 'input',
+                name: 'addRoleSalary',
+                message: "What is the salary of the role you would like to add?",
+                when: (answers) => answers['actions'] === 'addRoleName'
+
+            },
+            {
+                type: 'input',
+                name: 'addRoleDepartment',
+                message: "What department does the role belong to?",
+                when: (answers) => answers['actions'] === 'addRoleSalary'
+
+            },
+            {
+                type: 'input',
+                name: 'addEmployeeFName',
+                message: "What is the first name of the employee you would like to add?",
+                when: (answers) => answers['actions'] === 'Add Employee'
+
+            },
+            {
+                type: 'input',
+                name: 'addEmployeeLName',
+                message: "What is the last name of the employee you would like to add?",
+                when: (answers) => answers['actions'] === 'addEmployeeFName'
+
+            },
+            {
+                type: 'input',
+                name: 'addEmployeeRole',
+                message: "What is the last name of the employee you would like to add?",
+                when: (answers) => answers['actions'] === 'addEmployeeLName'
+
+            },
+            {
+                type: 'input',
+                name: 'addEmployeeManager',
+                message: "What is the last name of the employee you would like to add?",
+                when: (answers) => answers['actions'] === 'addEmployeeRole'
+
+            },
+
+
+            {
+                type: 'input',
+                name: 'updateEmployeeRole',
+                message: "What is the name of the department you would like to update the employee to?",
+                when: (answers) => answers['actions'] === 'Update Employee Role'
+
+            }
+        ]
+        )
+};
