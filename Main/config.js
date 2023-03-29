a87b0f8a3c8aed6a71f9803cae15b32da56886b8
bmfoster5 <foster.brandon96@gmail.com>
2023-03-29 17:25:30 -0400

    config and server maintence with error

--- Main/config.js
+++ Main/config.js
@@ -1,23 +1,8 @@
 require('dotenv').config()
-const express = require('express');
-// Import and require mysql2
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // MySQL username,
-      user: PROCESS.ENV.DB_USER,
-      // TODO: Add MySQL password here
-      password: process.env.DB_PASSWORD,
-      database: PROCESS.ENV.DB_NAME,
-    },
-    console.log(`Connected to the employe_db database.`)
-  );
+module.exports = {
+    host: process.env.DB_HOSTNAME,
+    user:process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  };
